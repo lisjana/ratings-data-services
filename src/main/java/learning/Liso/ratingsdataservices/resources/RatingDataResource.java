cfc31fba89990e4d7aad5d8caed089f588304eda
lisjana.allkja <lisjana.allkja@gmail.com>
2020-01-28 23:42:42 +0100

    Adding list as object

--- src/main/java/learning/Liso/ratingsdataservices/resources/RatingDataResource.java
+++ src/main/java/learning/Liso/ratingsdataservices/resources/RatingDataResource.java
@@ -1,9 +1,13 @@
 package learning.Liso.ratingsdataservices.resources;
 
 import learning.Liso.ratingsdataservices.models.Rating;
+import learning.Liso.ratingsdataservices.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +16,18 @@ public class RatingDataResource {
     @RequestMapping("/{movieId}")
     public Rating getRatinginfo(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getRatingInfo(@PathVariable("userId") String userId){
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("2345", 3),
+                new Rating("174734", 2)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
